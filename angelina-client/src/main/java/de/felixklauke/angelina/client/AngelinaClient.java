670f6388714aedb330db3023677ebdc40ea46423
Felix Klauke <info@felix-klauke.de>
2018-09-18 10:53:34 +0200

    Extract client functionality.

---
+++ angelina-client/src/main/java/de/felixklauke/angelina/client/AngelinaClient.java
@@ -0,0 +1,8 @@
+package de.felixklauke.angelina.client;
+
+/**
+ * @author Felix Klauke <info@felix-klauke.de>
+ */
+public class AngelinaClient {
+
+}
---
+++ angelina-core/src/main/java/de/felixklauke/angelina/core/AngelinaCore.java
@@ -0,0 +1,8 @@
+package de.felixklauke.angelina.core;
+
+/**
+ * @author Felix Klauke <info@felix-klauke.de>
+ */
+public class AngelinaCore {
+
+}
